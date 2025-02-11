f47b0c6d6c82c93fdcd84b35f65399dba28fbe7f
Aaron Tully <aarontully0@gmail.com>
2025-02-11 16:15:07 +1100

    test

--- src/taskpane/Js/signature_templates.js
+++ src/taskpane/Js/signature_templates.js
@@ -17,12 +17,24 @@ function get_template_A_str(user_info)
 
 function get_template_B_str(user_info)
 {
-  let str = `
-    <div>
-      ${is_valid_data(user_info.greeting) ? `<div style="padding-bottom: 16px;">${user_info.greeting}</div>` : ""}
-      <div style="color: #FF4370;"><strong>${user_info.name} ${is_valid_data(user_info.pronoun) ? `(${user_info.pronoun})` : ""}</strong></div>
-      <div style="padding-bottom: 16px;">${user_info.title} ${is_valid_data(user_info.department) ? ` | ${user_info.department}` : ""}</div>
-    </div>
-  `;
+  let str = "";
+  if (is_valid_data(user_info.greeting))
+  {
+    str += user_info.greeting + "<br/>";
+  }
+
+  str += "<table>";
+  str +=   "<tr>";
+  str +=     "<td style='border-right: 1px solid #000000; padding-right: 5px;'><img src='https://officedev.github.io/Office-Add-in-samples/Samples/outlook-set-signature/assets/sample-logo.png' alt='Logo' /></td>";
+  str +=     "<td style='padding-left: 5px;'>";
+  str +=	   "<strong>" + user_info.name + "</strong>";
+  str +=     is_valid_data(user_info.pronoun) ? "&nbsp;" + user_info.pronoun : "";
+  str +=     "<br/>";
+  str +=	   user_info.email + "<br/>";
+  str +=	   is_valid_data(user_info.phone) ? user_info.phone + "<br/>" : "";
+  str +=     "</td>";
+  str +=   "</tr>";
+  str += "</table>";
+
   return str;
 }
