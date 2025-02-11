3f9e462d2a7fa16903478701f76418ca77e77a63
Aaron Tully <aarontully0@gmail.com>
2025-02-11 16:17:06 +1100

    test2

--- src/taskpane/Js/signature_templates.js
+++ src/taskpane/Js/signature_templates.js
@@ -1,17 +1,24 @@
 function get_template_A_str(user_info)
 {
-  let str = `
-    <div>
-      ${is_valid_data(user_info.greeting) ? `<div style="padding-bottom: 16px;">${user_info.greeting}</div>` : ""}
-      <div style="color: #FF4370;"><strong>${user_info.name} ${is_valid_data(user_info.pronoun) ? `(${user_info.pronoun})` : ""}</strong></div>
-      <div style="padding-bottom: 16px;">${user_info.title} ${is_valid_data(user_info.department) ? ` | ${user_info.department}` : ""}</div>
-      ${is_valid_data(user_info.phone) ? `<div>${user_info.phone}</div>` : ""}
-      <div>${address}</div>
-      <div><a href="https://www.endemolshine.com.au/">www.endemolshine.com.au</a></div>
-      <div><img src="./assets/sig_image.png" alt="logo" /></div>
-      <div style="font-size: 10px;">This electronic mail, including any attachments, is intended for the addressee only and may contain information that is either confidential or subject to legal professional privilege. Unauthorised reproduction, use or disclosure of the contents of this mail is prohibited. If you have received this mail in error, please delete it from your system immediately and notify Endemol Shine Australia by contacting us at www.endemolshine.com.au</div>
-    </div>
-  `;
+  let str = "";
+  if (is_valid_data(user_info.greeting))
+  {
+    str += user_info.greeting + "<br/>";
+  }
+
+  str += "<table>";
+  str +=   "<tr>";
+  str +=     "<td style='border-right: 1px solid #000000; padding-right: 5px;'><img src='https://officedev.github.io/Office-Add-in-samples/Samples/outlook-set-signature/assets/sample-logo.png' alt='Logo' /></td>";
+  str +=     "<td style='padding-left: 5px;'>";
+  str +=	   "<strong>" + user_info.name + "</strong>";
+  str +=     is_valid_data(user_info.pronoun) ? "&nbsp;" + user_info.pronoun : "";
+  str +=     "<br/>";
+  str +=	   user_info.email + "<br/>";
+  str +=	   is_valid_data(user_info.phone) ? user_info.phone + "<br/>" : "";
+  str +=     "</td>";
+  str +=   "</tr>";
+  str += "</table>";
+
   return str;
 }
 
